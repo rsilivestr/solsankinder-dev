cc3f93349978fde1699d9ae174aae464cc7215c9
rsilivestr <rsilivestr@gmail.com>
2020-08-19 12:28:47 +0300

    Add card variant without photo

--- src/php/pages/cards.php
+++ src/php/pages/cards.php
@@ -3,27 +3,35 @@
 $cardsHTML = "";
 
 foreach($page->cards as $card) {
-  $imageHTML = "<img class='card-tile__image' src='{$config->urls->assets}images/no-photo.png' />";
+  $imageHTML = "";
 
-  if($card->cardPhoto) {
-    $xs = $card->cardPhoto->width('320');
-    $sm = $card->cardPhoto->width('450');
+  if (!$card->cardTextOnly) {
+    $imageHTML = "<img class='card-tile__image' src='{$config->urls->assets}images/no-photo.png' />";
 
-    $imageHTML = "<img class='card-tile__image lazy'
-      src='{$card->cardPhoto->url}'
-      srcset='{$config->urls->assets}images/no-photo.png'
-      data-srcset='
-        $xs->url 320w,
-        $sm->url 450w'
-      sizes='(max-width: 344px) 320px, 450px'
-      alt='$image->description'
-    >";
+    if($card->cardPhoto) {
+      $xs = $card->cardPhoto->width('320');
+      $sm = $card->cardPhoto->width('450');
+  
+      $imageHTML = "<img class='card-tile__image lazy'
+        src='{$card->cardPhoto->url}'
+        srcset='{$config->urls->assets}images/no-photo.png'
+        data-srcset='
+          $xs->url 320w,
+          $sm->url 450w'
+        sizes='(max-width: 344px) 320px, 450px'
+        alt='$image->description'
+      >";
+    }
   }
 
+  $imageContainerHTML = "";
+
+  if ($imageHTML !== "") {
+    $imageContainerHTML = "<div class='card-tile__image-container'>$imageHTML</div>";
+  }  
+
   $cardsHTML .= "<div class='card-tile'>
-    <div class='card-tile__image-container'>
-      $imageHTML
-    </div>
+    $imageContainerHTML
     <div class='card-tile__text-content'>
       $card->body
     </div>
