3bdb81832316036cd7a135c9111a8f05d27c950b
rsilivestr <rsilivestr@gmail.com>
2020-10-26 12:49:04 +0300

    Change admin panel styles

--- src/static/scripts/check-in-admin.js
+++ src/static/scripts/check-in-admin.js
@@ -19,12 +19,14 @@ const SolCheckInAdmin = (() => {
 
   const createTable = (events) => {
     const table = document.createElement('table');
+    table.className = 'ci-table';
     table.innerHTML = `<thead>
       <th>№</th>
       <th>ФИО</th>
       <th>Дата рождения</th>
       <th>Телефон</th>
       <th>Интервал</th>
+      <th>⨯</th>
     </thead>`;
 
     events.forEach((event, index) => {
@@ -32,9 +34,15 @@ const SolCheckInAdmin = (() => {
       row.innerHTML = `
         <td>${index + 1}</td>
         <td>${event.fio}</td>
-        <td>${event.dob}</td>
+        <td>${(new Date(event.dob)).toLocaleDateString('ru-RU')}</td>
         <td>${event.phone}</td>
         <td>${event.start_time.substring(0, 5)} - ${event.end_time.substring(0, 5)}</td>
+        <td>
+          <!--
+          <button class="ci-table__btn js-edit-event" data-id=${event.id}>🖉</button>
+          -->
+          <button class="ci-table__btn js-delete-event" data-id=${event.id}>⨯</button>
+        </td>
       `;
       table.appendChild(row);
     });
@@ -46,12 +54,12 @@ const SolCheckInAdmin = (() => {
     e.preventDefault();
 
     const date = UIformShowEvents.querySelector('input').value;
-
     const eventData = await fetchEventData(date);
-
     const tableHTML = createTable(eventData);
+    const tableWrap = document.querySelector('.ci-table-wrap');
 
-    document.querySelector('.ci-table-wrap').appendChild(tableHTML);
+    tableWrap.innerHTML = '';
+    tableWrap.appendChild(tableHTML);
   }
 
   return {
