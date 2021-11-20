7fb5d5def1f1798cf56da4e0c08dc071741d297c
Roman Silivestrov <roman.silivestrov@metalamp.io>
2021-11-20 17:01:45 +0300

    Add getVoucher entry point to webpack

--- src/php/pages/check-in__get-voucher.php
+++ src/php/pages/check-in__get-voucher.php
@@ -5,5 +5,5 @@ $content = '';
 $formHTML = file_get_contents('./check-in/get-voucher-form.html');
 $content .= $formHTML;
 
-$scriptName = $manifest['get-voucher.js'];
+$scriptName = $manifest['getVoucher.js'];
 $content .= "<script src='{$config->urls->templates}{$scriptName}'></script>";
