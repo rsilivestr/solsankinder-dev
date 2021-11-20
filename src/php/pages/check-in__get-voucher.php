cbbf3f59bed06bc6682c838ce8a503134a699d59
Roman Silivestrov <roman.silivestrov@metalamp.io>
2021-11-20 16:48:33 +0300

    Update page template, form html

--- src/php/pages/check-in__get-voucher.php
+++ src/php/pages/check-in__get-voucher.php
@@ -2,7 +2,7 @@
 
 $content = '';
 
-$formHTML = file_get_contents('./check-in/form.html');
+$formHTML = file_get_contents('./check-in/get-voucher-form.html');
 $content .= $formHTML;
 
 $scriptName = $manifest['get-voucher.js'];
