77321a1714dc8e7d43a67c3228a2af2eab6705ca
Roman Silivestrov <roman.silivestrov@metalamp.io>
2021-11-21 09:53:37 +0300

    Prettify check-in admin script

--- src/js/checkinAdmin.js
+++ src/js/checkinAdmin.js
@@ -1,10 +1,10 @@
 const SolCheckInAdmin = (() => {
   const UI = {
-    formShowEvents: document.getElementById('show-events-form'),
+    eventsForm: document.getElementById('show-events-form'),
     tableWrap: document.querySelector('.ci-table-wrap'),
   };
 
-  const fetchEventData = async (date, intervalId) => {
+  const fetchEvents = async (date, intervalId) => {
     const formData = new FormData();
     formData.append('ci_date', date);
     formData.append('interval_id', intervalId);
@@ -20,39 +20,47 @@ const SolCheckInAdmin = (() => {
     return data;
   };
 
-  const createTable = (events) => {
+  const createEventTable = (events) => {
     const table = document.createElement('table');
     table.className = 'ci-table';
     table.id = 'ci-table';
 
     table.innerHTML = `
-    <thead>
-      <th>№</th>
-      <th>ФИО</th>
-      <th>Дата рождения</th>
-      <th>Телефон</th>
-      <th>Отделение</th>
-      <th>Интервал</th>
-      <th>Район</th>
-      <th>Удалить</th>
-    </thead>`;
+      <thead>
+        <th>№</th>
+        <th>ФИО</th>
+        <th>Дата рождения</th>
+        <th>Телефон</th>
+        <th>Отделение</th>
+        <th>Интервал</th>
+        <th>Район</th>
+        <th>Удалить</th>
+      </thead>`;
 
     const tbody = document.createElement('tbody');
     table.appendChild(tbody);
 
     events.forEach((event, index) => {
+      const {
+        fio,
+        dob,
+        phone,
+        unit_name: unit,
+        start_time: startTime,
+        end_time: endTime,
+        district_name: district,
+        event_id: eventId,
+      } = event;
       const row = document.createElement('tr');
       row.innerHTML = `
         <td>${index + 1}</td>
-        <td>${event.fio}</td>
-        <td>${new Date(event.dob).toLocaleDateString('ru-RU')}</td>
-        <td>${event.phone}</td>
-        <td>${event.unit_name}</td>
-        <td>${event.start_time.substring(0, 5)} - ${event.end_time.substring(0, 5)}</td>
-        <td>${event.district_name}</td>
-        <td><button class="ci-table__btn js-delete-event" data-id=${
-          event.event_id
-        }>&#128473;</button></td>
+        <td>${fio}</td>
+        <td>${new Date(dob).toLocaleDateString('ru-RU')}</td>
+        <td>${phone}</td>
+        <td>${unit}</td>
+        <td>${startTime.substring(0, 5)} - ${endTime.substring(0, 5)}</td>
+        <td>${district}</td>
+        <td><button class="ci-table__btn js-delete-event" data-id=${eventId}>&#128473;</button></td>
       `;
       tbody.appendChild(row);
     });
@@ -68,12 +76,11 @@ const SolCheckInAdmin = (() => {
   const showEvents = async (e) => {
     e.preventDefault();
 
-    const date = UI.formShowEvents.querySelector('.js-search-date-input').value;
-    const intervalId = UI.formShowEvents.querySelector('.js-filter-interval-input').value || '0';
+    const date = UI.eventsForm.querySelector('.js-search-date-input').value;
+    const intervalId = UI.eventsForm.querySelector('.js-filter-interval-input').value || '0';
 
-    const eventData = await fetchEventData(date, intervalId);
-
-    const tableHTML = createTable(eventData);
+    const eventData = await fetchEvents(date, intervalId);
+    const tableHTML = createEventTable(eventData);
 
     UI.tableWrap.innerHTML = '';
     UI.tableWrap.appendChild(tableHTML);
@@ -86,7 +93,7 @@ const SolCheckInAdmin = (() => {
     if (!target.classList.contains('js-delete-event')) return;
 
     const check = prompt('Для подтверждения введите "удалить"');
-    if ('удалить' !== check) return;
+    if (check !== 'удалить') return;
 
     const body = new FormData();
     body.append('id', target.dataset.id);
@@ -97,18 +104,18 @@ const SolCheckInAdmin = (() => {
       body,
     });
 
-    const data = await res.json();
+    const { status, message } = await res.json();
 
-    if ('success' === data.status) {
+    if (status === 'success') {
       target.closest('tr').remove();
     } else {
-      console.log(data.message);
+      console.log(message);
     }
   };
 
   return {
     init: () => {
-      UI.formShowEvents.addEventListener('submit', showEvents);
+      UI.eventsForm.addEventListener('submit', showEvents);
       UI.tableWrap.addEventListener('click', deleteEvent);
     },
   };
